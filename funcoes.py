6395b5d64ea819e7f35865cb3430bfbab307ca56
JPNoman <jpfnoman@gmail.com>
2024-10-19 13:53:42 -0300

    Exercício 1

--- funcoes.py
+++ funcoes.py
@@ -1,1 +1,15 @@
-
+def define_posicoes(linha, coluna, orientacao, tamanho):
+  posição = []
+  if orientacao == 'vertical':
+    i = 0
+    while i!=tamanho:
+      linha +=i
+      posição.append([linha, coluna])
+      i+=1
+  else:
+    i = 0
+    while i!=tamanho:
+      coluna +=i
+      posição.append([linha, coluna])
+      i+=1
+  return posição
