b558c3f576e8849782a809963ef013125e13a13d
JPNoman <jpfnoman@gmail.com>
2024-10-19 16:10:02 -0300

    Exercício 2

--- funcoes.py
+++ funcoes.py
@@ -1,5 +1,6 @@
 def define_posicoes(linha, coluna, orientacao, tamanho):
   posição = []
+  x = []
   if orientacao == 'vertical':
     i = 0
     while i!=tamanho:
@@ -12,11 +13,11 @@ def define_posicoes(linha, coluna, orientacao, tamanho):
         posição.append([linha, coluna])
         coluna+=1      
         i+=1
-  return posição
+  x.append(posição)
+  return x
 
 def preenche_frota(frota, nome_navio, linha, coluna, orientacao, tamanho):
-  frota_update = {}
-  position = define_posicoes(linha, coluna, orientacao, tamanho)
-  frota_update[nome_navio] = frota[nome_navio]
-  frota_update[nome_navio] += position
-  return frota_update
+  for chave, valor in frota.items():
+    position = define_posicoes(linha, coluna, orientacao, tamanho)
+    frota[nome_navio] += position
+  return frota
