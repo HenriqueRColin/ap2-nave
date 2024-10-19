68ebb9c7dae60f4e1470d59d1450d0696e2eda0b
JPNoman <jpfnoman@gmail.com>
2024-10-19 15:32:23 -0300

    Exercício 2

--- funcoes.py
+++ funcoes.py
@@ -13,3 +13,10 @@ def define_posicoes(linha, coluna, orientacao, tamanho):
         coluna+=1      
         i+=1
   return posição
+
+def preenche_frota(frota, nome_navio, linha, coluna, orientacao, tamanho):
+  frota_update = {}
+  position = define_posicoes(linha, coluna, orientacao, tamanho)
+  frota_update[nome_navio] = frota[nome_navio]
+  frota_update[nome_navio] += position
+  return frota_update
