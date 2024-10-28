81d766c620a8cd38043028ead5631d95eeb0f5e8
HRColin <r.henriquecolin@gmail.com>
2024-10-27 23:57:23 -0300

    Correções

--- funcoes.py
+++ funcoes.py
@@ -1,32 +1,23 @@
-# Exercício 1
 def define_posicoes(linha, coluna, orientacao, tamanho):
-  posição = []
-  x = []
-  if orientacao == 'vertical':
-    i = 0
-    while i!=tamanho:
-      posição.append([linha, coluna])
-      linha +=1
-      i+=1
-  else:
-    i = 0
-    while i!=tamanho:
-        posição.append([linha, coluna])
-        coluna+=1      
-        i+=1
-  x.append(posição)
-  return x
+    posicoes = []
+    if orientacao == 'vertical':
+        for i in range(tamanho):
+            posicoes.append([linha + i, coluna])
+    else:
+        for i in range(tamanho):
+            posicoes.append([linha, coluna + i])
+    return posicoes 
 
 # Exercício 2
 def preenche_frota(frota, nome_navio, linha, coluna, orientacao, tamanho):
-  if nome_navio in frota:
-    position = frota[nome_navio]+define_posicoes(linha, coluna, orientacao, tamanho)
-    frota[nome_navio] = position
-  else:
-    frota[nome_navio] = []
-    position = frota[nome_navio]+define_posicoes(linha, coluna, orientacao, tamanho)
-    frota[nome_navio] = position
-  return frota  
+    nova_posicao = define_posicoes(linha, coluna, orientacao, tamanho)
+    if nome_navio in frota:
+        frota[nome_navio].append(nova_posicao)
+    else:
+        frota[nome_navio] = [nova_posicao]
+
+    return frota
+
 
 # Exercício 3
 def faz_jogada(tabuleiro, linha, coluna):
